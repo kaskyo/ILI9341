fa040003cbd80b5935afd1d4f4af1ab9bf430f35
JGDger <jgd_ger@mail.ru>
2018-07-21 18:17:01 +0300

    new uart ebanarot

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 
 #include <stdlib.h>
 
+#include <termios.h>		//Used for UART
 
 unsigned char *rgb565_buffer;
 
@@ -195,6 +196,50 @@ void readJPG(char* fn) {
 	free(bmp_buffer);
 }
 
+int uart0_filestream = -1;
+void InitUART()
+{
+	
+	//OPEN THE UART
+	//The flags (defined in fcntl.h):
+	//	Access modes (use 1 of these):
+	//		O_RDONLY - Open for reading only.
+	//		O_RDWR - Open for reading and writing.
+	//		O_WRONLY - Open for writing only.
+	//
+	//	O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
+	//											if there is no input immediately available (instead of blocking). Likewise, write requests can also return
+	//											immediately with a failure status if the output can't be written immediately.
+	//
+	//	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
+	uart0_filestream = open("/dev/serial0", O_WRONLY | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
+	if (uart0_filestream == -1)
+	{
+		//ERROR - CAN'T OPEN SERIAL PORT
+		printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
+	}
+	
+	//CONFIGURE THE UART
+	//The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
+	//	Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
+	//	CSIZE:- CS5, CS6, CS7, CS8
+	//	CLOCAL - Ignore modem status lines
+	//	CREAD - Enable receiver
+	//	IGNPAR = Ignore characters with parity errors
+	//	ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for bianry comms!)
+	//	PARENB - Parity enable
+	//	PARODD - Odd parity (else even)
+	struct termios options;
+	tcgetattr(uart0_filestream, &options);
+	options.c_cflag = B2000000 | CS8 | CLOCAL;		//<Set baud rate
+	options.c_iflag = IGNPAR;
+	options.c_oflag = 0;
+	options.c_lflag = 0;
+	tcflush(uart0_filestream, TCIFLUSH);
+	tcsetattr(uart0_filestream, TCSANOW, &options);	
+}
+
+
 int main()
 {/*
 	printf("Initing...");
@@ -212,7 +257,8 @@ int main()
 	//HAL_Delay(500);
 	//system("stty -F /dev/serial0 2000000");
 	HAL_Delay(1000);
-*/	FILE* uart = fopen("/dev/serial0","wb");
+*/	//FILE* uart = fopen("/dev/serial0","wb");
+	InitUART();
 	//FILE* jpeg;
 	//char* buffer;
 	char beacon[8] = { 'P', 'e', 't', 'o', 'u', 'c', 'h', '\0' };
@@ -245,7 +291,14 @@ int main()
 		fclose(jpeg); // Close the file
 		*/
 		char lol = 'U';
-		fwrite((const void*) &lol,1,1,uart);
+		//fwrite((const void*) &lol,1,1,uart);
+		
+		int count = write(uart0_filestream, &lol, 1);		//Filestream, bytes to write, number of bytes to write
+		if (count < 0)
+		{
+			printf("UART TX error\n");
+		}
+		
 		//fwrite((const void*) &beacon,8,1,uart);
 		HAL_Delay(500);
 		/*fwrite((const void*) &filelen,sizeof(int),1,uart);
