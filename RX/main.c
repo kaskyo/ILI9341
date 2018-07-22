e9ff7b5387b7c6cbb6443c4a552d7f580a7e889b
JGDger <jgd_ger@mail.ru>
2018-07-23 01:37:20 +0300

    morning

--- RX/main.c
+++ RX/main.c
@@ -359,14 +359,14 @@ int main()
 			read(uart0_filestream, (void*)&jpegSize, sizeof(uint16_t));
 			//printf("Size: %d\n", jpegSize);
 			uint16_t i = 0;
-			//FILE* fp=fopen("rx.jpg", "wb");
+			FILE* fp=fopen("rx.jpg", "wb");
 			while (i < jpegSize) {
 				rxl = read(uart0_filestream, jpegBuffer + i, jpegSize - i);
-				//fwrite(jpegBuffer + i, sizeof(char), rxl, fp); 
+				fwrite(jpegBuffer + i, sizeof(char), rxl, fp); 
 				//syslog(LOG_INFO, "Input: Read %d/%lu bytes", rc, jpg_size-i);
 				i += rxl;
 			}
-			//fclose(fp);
+			fclose(fp);
 			//rxl = read(uart0_filestream, (void*)jpegBuffer, jpegSize);
 			//printf("Read %d bytes\n",i);
 			if (jpegSize>0) {
@@ -374,7 +374,7 @@ int main()
 					ILI9341_Draw_Image((const char*)rgb565_buffer,SCREEN_HORIZONTAL_2);
 				
 			}
-			tcflush(uart0_filestream, TCIFLUSH);
+			
 		}
 		/*printf("Reading JPG...");
 		readJPG("/home/pi/ILI9341/1.jpg");
