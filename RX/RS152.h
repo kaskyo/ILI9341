80a3a0c2ffb323c8be946180ac2fbcaf894127c5
kaskyo <andruha40k@gmail.com>
2018-08-04 13:57:45 +0400

    Added RS152 to TX (kinda)

---
+++ RX/RS152.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void GenerateInttables();
+uint64_t RS152Code(uint8_t byte);
+uint8_t RS152Decode(uint64_t w);
--- TX/main.c
+++ TX/main.c
@@ -9,109 +9,14 @@
 #include <syslog.h>
 #include <sys/stat.h>
 
-#include <jpeglib.h>
 #include <stdlib.h>
 #include <termios.h>		//Used for UART
 #include <time.h>
 #include <string.h>
 
-#define BAUDRATE B500000
-//#define ham
+#include "RS152.h"
 
-unsigned char *rgb565_buffer;
-
-#ifdef ham
-uint8_t Code(uint8_t I) {
-  return I^(I<<1)^(I<<3);
-} 
-
-uint16_t Code8 (uint8_t G)
-{
-	uint8_t t1 = Code ( G & 0x0f);
-	uint8_t t2 = Code ( G >> 4 & 0x0f);
-	uint16_t t = t1 | (t2 << 8);
-	return t;
-}
-#endif
-
-void readJPG(char* fn) {
-	
-	int rc, i, j;
-	struct stat file_info;
-	unsigned long jpg_size;
-	unsigned char *jpg_buffer;
-	struct jpeg_decompress_struct cinfo;
-	struct jpeg_error_mgr jerr;
-	int row_stride, width, height, pixel_size;
-	unsigned long bmp_size;
-	unsigned char *bmp_buffer;
-
-	rc = stat(fn, &file_info);
-	if (rc) {
-		exit(EXIT_FAILURE);
-	}
-	jpg_size = file_info.st_size;
-	jpg_buffer = (unsigned char*) malloc(jpg_size + 100);
-
-	int fd = open(fn, O_RDONLY);
-	i = 0;
-	while (i < jpg_size) {
-		rc = read(fd, jpg_buffer + i, jpg_size - i);
-		i += rc;
-	}
-	close(fd);
-
-	cinfo.err = jpeg_std_error(&jerr);	
-	jpeg_create_decompress(&cinfo);
-	jpeg_mem_src(&cinfo, jpg_buffer, jpg_size);
-	rc = jpeg_read_header(&cinfo, TRUE);
-
-	if (rc != 1) {
-		exit(EXIT_FAILURE);
-	}
-
-	jpeg_start_decompress(&cinfo);
-	width = cinfo.output_width;
-	height = cinfo.output_height;
-	pixel_size = cinfo.output_components;
-	bmp_size = width * height * pixel_size;
-	bmp_buffer = (unsigned char*) malloc(bmp_size);
-	row_stride = width * pixel_size;
-
-	while (cinfo.output_scanline < cinfo.output_height) {
-		unsigned char *buffer_array[1];
-		buffer_array[0] = bmp_buffer + \
-						   (cinfo.output_scanline) * row_stride;
-
-		jpeg_read_scanlines(&cinfo, buffer_array, 1);
-
-	}
-
-	jpeg_finish_decompress(&cinfo);
-	jpeg_destroy_decompress(&cinfo);
-	free(jpg_buffer);
-	
-	unsigned char r,g,b,r5,g6,b5,bt1,bt2;
-
-	
-	for (uint32_t i=0; i<width*height; i++)
-	{
-		r = bmp_buffer[i*3];
-		g = bmp_buffer[i*3+1];
-		b = bmp_buffer[i*3+2];
-		r5 = (uint8_t)((uint16_t)r*31/255);
-		g6 = (uint8_t)((uint16_t)g*63/255);
-		b5 = (uint8_t)((uint16_t)b*32/255);
-		
-		bt1 = (r5 << 3) | ((g6 >> 3) & 0x07);
-		bt2 = ((g6 << 5) & 0xE0) | (b5 & 0x1F);
-		
-		rgb565_buffer[i*2] = bt1;
-		rgb565_buffer[i*2+1] = bt2;
-	}
-
-	free(bmp_buffer);
-}
+#define BAUDRATE B1000000
 
 int uart0_filestream = -1;
 void InitUART()
@@ -137,12 +42,11 @@ int main()
 	//system("stty -F /dev/serial0 2000000");
 */	//FILE* uart = fopen("/dev/serial0","wb");
 	InitUART();
+	GenerateInttables();
 	FILE* jpeg;
 	uint8_t* buffer;
-	#ifdef ham
-	uint8_t* bufferH;
-	uint8_t temp1, temp2;
-	#endif
+	uint64_t* bufferRS;
+	
 	uint8_t beacon[8] = { 'P', 'e', 't', 'o', 'u', 'c', 'h', '\0' };
 	for (;;) {
 
@@ -156,27 +60,20 @@ int main()
 		//begin = clock();
 		jpeg = fopen("/home/pi/ILI9341/1.jpg","rb");
 		fseek(jpeg, 0, SEEK_END);          // Jump to the end of the file
+		
 		uint16_t filelen = ftell(jpeg);
-		#ifdef ham
-			uint16_t filelenH = filelen*2;		// Get the current byte offset in the file		
-			uint32_t filelenHx = 0;
-		#endif
+		
 		rewind(jpeg);                      // Jump back to the beginning of the file
 
 		//printf("Filelen: %d\n",filelen);
-		buffer = (unsigned char *)malloc(filelen+100); // Enough memory for file + \0
-		#ifdef ham
-			bufferH = (unsigned char *)malloc(filelenH+100);
-		#endif
+		buffer = (uint8_t*)malloc(filelen+100); // Enough memory for file + \0
+		bufferRS = (uint64_t*)malloc(filelen*8+96);
 		fread(buffer, filelen, 1, jpeg); // Read in the entire file
 		fclose(jpeg); // Close the file
 		#ifdef ham
 			for (uint16_t i=0; i<filelen; i++)
 			{	
-				temp1 = Code(buffer[i]&0x0f);
-				temp2 = Code((buffer[i]&0xf0)>>4);
-				bufferH[i*2] = temp1;
-				bufferH[i*2+1] = temp2;
+				bufferRS[i] = RS152Code(buffer[i]);
 			}
 		#else
 			
@@ -190,24 +87,24 @@ int main()
 		//fwrite((const void*) &lol,1,1,uart);
 		
 		//begin = clock();
-		write(uart0_filestream, (const void*) &beacon,8);
+		
+		// Encode beacon too
+		for (int i=0; i<8; i++)
+			write(uart0_filestream, (const void*) RS152Code(beacon[i]),8);
 		//HAL_Delay(500);
-		#ifdef ham
-		for (uint8_t h = 0; h < 16; h=h+8)
-		{
-			filelenHx = Code8((filelenH >> h));
-			write(uart0_filestream, (const void*) &filelenHx,sizeof(uint16_t));
-		}
-		#else
-			write(uart0_filestream, (const void*) &filelen,sizeof(uint16_t));
-		#endif
+		
+		write(uart0_filestream, (const void*) RS152Code((filelen&0xff00)>>8),8);
+		write(uart0_filestream, (const void*) RS152Code(filelen&0xff),8);
+		
 		FILE* out = fopen ("tx.jpg","wb");
 		uint16_t j=0, wrl;
-		#ifdef ham
-			while (j<filelenH)
+		
+		uint8_t* buf1 = (uint8_t*)bufferRS;
+		
+			while (j<filelen*8)
 			{
-				wrl = write(uart0_filestream, bufferH + j,filelenH - j);
-				fwrite(bufferH + j, sizeof(uint8_t),wrl,out);
+				wrl = write(uart0_filestream, bufferRS + j,filelenH - j);
+				//fwrite(bufferH + j, sizeof(uint8_t),wrl,out);
 				j += wrl;
 			}
 		#else
@@ -222,11 +119,9 @@ int main()
 		//end = clock();
 		//timespent =(double)(end-begin)/(CLOCKS_PER_SEC/1000);
 		//printf("Sent. (in %f ms)\n\n",timespent);
+		buf1 = NULL;
 		free(buffer);
-		#ifdef ham
-			free(bufferH);
-		#endif
+		free(bufferRS);
 	}
-	free(rgb565_buffer);
 	//fclose(uart);
 }
